51f6b9c5dde4138276d93738e55f21097828d6df
Yannis Kontos <unreal810@gmail.com>
2023-07-20 03:50:15 +0100

    finished

--- app.js
+++ app.js
@@ -3,15 +3,32 @@ const topOperators = document.querySelectorAll('.top-operators');
 const sideOperators = document.querySelectorAll('.side-operators');
 const allClear = document.getElementById('allClear');
 const output = document.getElementById('output');
+const display = document.getElementById('display');
 let isFirstNum;
+let expression = [];
 
 class Calculator {
     prevNum = '';
+    operation = '';
 
     constructor(currentNum) {
         this.currentNum = currentNum;
         this.clearScreen();
-        console.log(this)
+    }
+
+    extraOperations(operation) {
+        switch(operation) {
+            case 'c':
+                this.clearScreen();
+                break;
+            case 'backspace':
+                if (this.currentNum.innerText === '0' || this.currentNum.innerText.length === 0) this.currentNum.innerText = '0', isFirstNum = true
+                else {this.currentNum.innerText = this.currentNum.innerText.slice(0, -1)};
+                break;
+            case '%':
+                this.simpleNumMethods('percentage');
+                break;
+        }
     }
 
     simpleNumMethods(operation) {
@@ -23,36 +40,65 @@ class Calculator {
             case 'negateNum':
                 if(this.currentNum.innerText === '0') return;
                 this.currentNum.innerText = parseFloat(-this.currentNum.innerText);
+                this.prevNum = this.currentNum.innerText;
                 break;
 
             case 'percentage':
-                this.currentNum = this.currentNum.innerText / 100;
+                if(this.currentNum.innerText === '0') return;
+                let current = new Decimal(this.currentNum.innerText);
+                this.currentNum.innerText = current.dividedBy(100);
+                this.prevNum = this.currentNum.innerText;
                 break;
         }
     }
 
     chooseOperation(operation) {
         isFirstNum = true;
+        if (expression.length === 2) { this.compute() };
         this.prevNum = this.currentNum.innerText;
-        console.log(this, operation);
-        this.currentNum
+        this.operation = operation;
     }
     
-    compute(operation) {
+    compute() {
+        let sum;
+        let prev = new Decimal(expression[0]);
+        let current = new Decimal(this.currentNum.innerText);
 
+        switch(this.operation) {
+            case 'divide':
+                sum = prev.dividedBy(current);
+                break;
+            case 'multiply':
+                sum = prev.times(current);
+                break;
+            case 'minus':
+                sum = prev.minus(current);
+                break;
+            case 'addition':
+                sum = prev.plus(current);
+                break;
+        }
+        this.currentNum.innerText = sum;
+        this.prevNum = '';
+        expression = [];
     }
 
     clearScreen() {
         this.currentNum.innerText = '0';
         allClear.textContent = 'AC';
+        this.prevNum = '';
         isFirstNum = true;
+        expression = [];
+        output.style.fontSize = '2.8rem';
     }
 
     displayNum(inputtedNum, isFirstNum) {
-        if (this.currentNum.innerText.includes('.') && inputtedNum === '.') return;
-        if (isFirstNum && inputtedNum != '.') { this.currentNum.innerText = inputtedNum; }
-        else {this.currentNum.innerText += inputtedNum; };
-        console.log(this);
+        if (this.currentNum.innerText.includes('.') && inputtedNum === '.' && !isFirstNum) return;
+        isFirstNum && inputtedNum != '.' ?  this.currentNum.innerText = inputtedNum : this.currentNum.innerText += inputtedNum;
+        if (isFirstNum && inputtedNum === '.') {this.currentNum.innerText = '0.'}
+        if (this.operation !== '' && this.prevNum !== '') expression.push(this.prevNum, this.operation), this.prevNum = '', output.style.fontSize = '2.8rem';
+        if (expression.includes('equals')) expression = [];
+        if (output.clientWidth === 213) output.style.fontSize = `calc(${2.2 / (output.textContent.length / 10)}rem)`;
     }
 }
 
@@ -76,4 +122,21 @@ sideOperators.forEach((sideOperator) => {
     sideOperator.addEventListener('click', e => {
         calc.chooseOperation(e.target.id);
     });
+});
+
+document.addEventListener('keydown', e => {
+    const inputtedKey = e.key;
+    const validKeys = ['1','2','3','4','5','6','7','8','9','0', '.'];
+    const operationKeys = ['addition', 'minus', 'divide', 'multiply', 'equals', 'equals'];
+    const operations = ['+', '-', '/', '*', '=', 'Enter'];
+    const extraOperations = ['c', 'backspace', '%'];
+    const operationIndex = operations.indexOf(inputtedKey);
+
+    if (validKeys.includes(inputtedKey)) {
+        allClear.textContent = 'C';
+        calc.displayNum(inputtedKey, isFirstNum);
+        output.textContent != '0' ?  isFirstNum = false : false;
+    }
+    else if (operationIndex !== -1) {calc.chooseOperation(operationKeys[operationIndex])}
+    else if (extraOperations.includes(inputtedKey.toLowerCase())) calc.extraOperations(inputtedKey.toLowerCase());
 });
